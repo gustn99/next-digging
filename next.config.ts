64d7e772eb5b29c0bfa9c36220477545eadd0d89
gustn99 <azerosong@gmail.com>
2026-07-14 16:53:28 +0900

    chore: pwa 플러그인 설치 및 설정 추가

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,18 @@
 import type { NextConfig } from "next";
+import withPWAInit from "@ducanh2912/next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  disable: false, // For testing PWA in local development
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  turbopack: {},
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
