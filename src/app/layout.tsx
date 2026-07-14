bb2f426f64e887c51478604edf6c858c5c8abf18
gustn99 <azerosong@gmail.com>
2026-07-15 00:29:02 +0900

    fix: PWA 앱 속성 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,45 +1,50 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type {Metadata, Viewport} from 'next';
+import {Geist, Geist_Mono} from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: '--font-geist-sans',
+	subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: '--font-geist-mono',
+	subsets: ['latin'],
 });
 
-import type { Viewport } from "next";
 export const metadata: Metadata = {
-  title: "Digging Archive",
-  description: "쇼핑몰 상품과 링크를 쉽게 아카이빙하세요.",
-  manifest: "/manifest.json",
-  icons: {
-    apple: "/icon-192x192.png",
-  },
+	title: 'Digging',
+	description: '쇼핑몰 상품과 링크를 쉽게 아카이빙하세요.',
+	manifest: '/manifest.json',
+	icons: {
+		apple: '/icon-192x192.png',
+	},
+	appleWebApp: {
+		capable: true,
+		statusBarStyle: 'default',
+		title: 'Digging',
+	},
+	applicationName: 'Digging',
 };
 
 export const viewport: Viewport = {
-  themeColor: "#18181b",
-  width: "device-width",
-  initialScale: 1,
-  maximumScale: 1,
+	themeColor: '#18181b',
+	width: 'device-width',
+	initialScale: 1,
+	maximumScale: 1,
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html
-      lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
-    >
-      <body className="min-h-full flex flex-col">{children}</body>
-    </html>
-  );
+	return (
+		<html
+			lang="en"
+			className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+		>
+		<body className="min-h-full flex flex-col">{children}</body>
+		</html>
+	);
 }
