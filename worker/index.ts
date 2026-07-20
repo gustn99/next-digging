208ac66dbe30cff8f385d69f2cc83d2d019043ab
gustn99 <azerosong@gmail.com>
2026-07-20 14:27:19 +0900

    feat: 푸시 알림 뱃지 아이콘 추가

--- worker/index.ts
+++ worker/index.ts
@@ -10,8 +10,8 @@ self.addEventListener('push', (event: any) => {
     // 알림 옵션 설정
     const options = {
       body: data.body,
-      icon: '/icon-192x192.png', // public 폴더에 있는 PWA 아이콘 경로 (프로젝트에 맞게 변경하세요)
-      badge: '/icon-192x192.png',
+      icon: '/icon-192x192.png', // 푸시 알림 내용 옆에 뜨는 큰 아이콘 (컬러 가능)
+      badge: '/badge-icon.png', // 안드로이드 상태바에 뜨는 작은 아이콘 (반드시 배경이 투명한 흰색 단색 이미지여야 함, 72x72 또는 96x96 권장)
       vibrate: [100, 50, 100],
       data: {
         url: '/' // 항상 루트 페이지로 이동
